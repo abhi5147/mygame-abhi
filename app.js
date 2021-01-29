0581971e2572d040162a971a4d13e275dda9b774
Abhi <ak14502@gmail.com>
2021-01-30 00:23:06 +0530

    host

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { url } = require("inspector");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 8000;
 
 
 // EXPRESS SPECIFIC STUFF
